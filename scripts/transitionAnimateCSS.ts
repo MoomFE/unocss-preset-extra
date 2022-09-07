28b759c51eb1bdfa7f9d6b3878279fe093daaf41
倚窗听雨丿醉无心 <994039348@qq.com>
2022-09-07 18:02:03 +0800

    读取 `animate.css` 样式写入到 JSON 文件中, 方便调用

---
+++ scripts/transitionAnimateCSS.ts
@@ -0,0 +1,36 @@
+import { resolve } from 'path';
+import { outputFileSync, readFileSync } from 'fs-extra';
+import { getPackageInfoSync } from 'local-pkg';
+import fg from 'fast-glob';
+import postcss from 'postcss';
+import postcssJs from 'postcss-js';
+
+const rootPath = getPackageInfoSync('animate.css')!.rootPath;
+const cssPaths = fg.sync(['source/*/*.css'], { cwd: rootPath }).map((path) => {
+  return resolve(rootPath, path);
+});
+
+const styles = Object.fromEntries(
+  cssPaths.map((path) => {
+    const code = readFileSync(path, { encoding: 'utf-8' });
+    const rules = Object.entries(
+      postcssJs.objectify(postcss.parse(code)),
+    );
+
+    const [name, css] = rules.find(([key]) => key.startsWith('.'))!;
+    const keyframes = rules.find(([key]) => key.startsWith('@'))![1];
+
+    return [
+      name.replace(/^(\.animated)?\./, ''),
+      {
+        css,
+        keyframes,
+      },
+    ];
+  }),
+);
+
+outputFileSync(
+  resolve(__dirname, '../src/rules/animated.json'),
+  JSON.stringify(styles, null, 2),
+);
